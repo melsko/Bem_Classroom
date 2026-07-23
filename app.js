75d7472305e5c5eeafa74f33f766432753610673
Melsko <mcphfaye@gmail.com>
2026-07-24 01:02:33 +0200

    Containerize the app

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Pipeline works! Container is alive.\n');
+});
+server.listen(3000, () => console.log('Server listening on port 3000'));
+
